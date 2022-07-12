dd94cf291f723255558f3495a6c82c6aea000a8e
MSBivens <mikebiv95@gmail.com>
2022-07-12 02:47:54 -0500

    cleanup

--- portfolio/pages/portfolio.js
+++ portfolio/pages/portfolio.js
@@ -1,9 +1,11 @@
-import Head from "next/head";
+import React from "react";
 
-export default function portfolio() {
+const portfolio = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8"></div>
     </div>
   );
-}
+};
+
+export default portfolio;
