2560a784378f94d1771ee0aaef8238040febde17
MSBivens <mikebiv95@gmail.com>
2022-10-18 16:23:55 -0500

    updating

--- portfolio/components/Sidebar.jsx
+++ portfolio/components/Sidebar.jsx
@@ -55,12 +55,10 @@ const Sidebar = () => {
         <p className="my-2">Contact Via Socials</p>
       </div>
 
-      {/* Email Button */}
-
       <button className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none">
         Code
       </button>
-      <button className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none">
+      <button className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none">
         Coaching
       </button>
       <button
