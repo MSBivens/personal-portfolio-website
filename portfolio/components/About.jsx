3ec542742c8c77e11500c7ce017a4f74ebd30135
MSBivens <mikebiv95@gmail.com>
2022-10-19 17:08:04 -0500

    updated description

--- portfolio/components/About.jsx
+++ portfolio/components/About.jsx
@@ -6,7 +6,11 @@ const About = () => {
   return (
     <div className="flex flex-col flex-grow px-6 pt-1 ">
       <h6 className="my-3 text-base font-medium">
-        I am currently BUIDLing. Check out my active efforts below!
+        I&#39;m a blockend developer and skills coach. I code smart contracts
+        and research their applications with DAO&#39;s and digital assets. My
+        extensive background in building and leading skills development allows
+        me to share this experience with others. Check out my active efforts
+        below!
       </h6>
       <div
         className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
